25bc9abc7e95a05fdf791fa059a8bd9dc002d58d
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-09-28 11:59:40 +0200

    Add project files.

---
+++ CalcolatriceSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalcolatriceSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
