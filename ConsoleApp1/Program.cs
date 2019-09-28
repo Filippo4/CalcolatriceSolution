a7990f7a26654834acdc19b28425ad50a5fecdf5
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-09-28 12:15:47 +0200

    ho creato una Creazione di un progetto in console che saluta il mondo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace CalcolatriceSolution
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Franco");
         }
     }
 }
